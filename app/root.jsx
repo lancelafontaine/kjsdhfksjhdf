d3d8c6798348ec497342402c9ac21a1d3a6cab61
Lance Lafontaine <lance.lafontaine@shopify.com>
2023-04-04 09:32:02 -0400

    Fixes

--- app/root.jsx
+++ app/root.jsx
@@ -19,6 +19,8 @@ import {seoPayload} from '~/lib/seo.server';
 import {DEFAULT_LOCALE, parseMenu} from './lib/utils';
 import invariant from 'tiny-invariant';
 import {useAnalytics} from './hooks/useAnalytics';
+import {Sidebar} from '@shopify/online-store-ui';
+import {ChakraProvider} from "@chakra-ui/react"
 
 export const links = () => {
   return [
